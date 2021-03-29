80f594a0c70b004963f2985279fd3307f04e96c5
Vasily Kamagorov <likanich152@gmail.com>
2021-03-29 10:04:54 +0700

    spring annotation file

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,10 @@
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloWorld {
     public static void main(String... args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/app-context.xml");
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
         MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
         mr.render();
     }
---
+++ src/main/java/HelloWorldConfiguration.java
@@ -0,0 +1,18 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+
+    @Bean
+    public MessageProvider provider() {
+        return new HelloWorldMessageProvider();
+    }
+
+    @Bean
+    public MessageRenderer renderer() {
+        MessageRenderer renderer = new StandartOutMessageRenderer();
+        renderer.setMessageProvider(provider());
+        return renderer;
+    }
+}
